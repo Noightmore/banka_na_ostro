0c93977d2a9251da05aed2cafec68cd13bf6e09d
john.doe <robert.hak@tul.cz>
2023-04-14 18:12:42 +0200

    fixed bad test

--- backend/tests/TestModel.cpp
+++ backend/tests/TestModel.cpp
@@ -7,9 +7,9 @@ using namespace bank::models;
 TEST(BalanceTest, CreateInstance)
 {
         auto name = std::make_unique<std::string>("USD");
-        auto amount = std::make_unique<double>(1000);
+        auto amount = std::make_unique<double>(1000.000000);
         auto balance = Balance::createInstance(std::move(name), std::move(amount));
-        ASSERT_EQ(balance->toJson(), R"({"name": "USD","amount": 1000})");
+        ASSERT_EQ(balance->toJson(), R"({"name": "USD","amount": 1000.000000})");
 }
 
 TEST(BalanceTest, CreateInstanceWithEmptyName)
