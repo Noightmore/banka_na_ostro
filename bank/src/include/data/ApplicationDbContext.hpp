23ff4f1a24210f00661018e74d1fe9ca58d2c610
john.doe <robert.hak@tul.cz>
2023-04-15 18:47:09 +0200

    added loicense

--- bank/src/include/data/ApplicationDbContext.hpp
+++ bank/src/include/data/ApplicationDbContext.hpp
@@ -2,11 +2,20 @@
 #define BACKEND_APPLICATIONDBCONTEXT_HPP
 
 
+#include "include/data/models/BankData.hpp"
+
 namespace bank::data
 {
+    /**
+     * @brief The main class for the database context
+     *
+     * @details this class is managing all read and write operations to the database
+     *          in this case it is a simple xml file local database located in the bank/db directory
+     *          also provides the bank data to the whole application
+     */
     class ApplicationDbContext
     {
-
+        models::BankData *bankData;
     };
 }
 
