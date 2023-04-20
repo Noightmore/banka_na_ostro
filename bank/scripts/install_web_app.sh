2558c7b2226ec2a9d5292858061215322b80db88
robert <robert.hak@tul.cz>
2023-04-20 11:06:26 +0200

    apache server config update 2

--- bank/scripts/install_web_app.sh
+++ bank/scripts/install_web_app.sh
@@ -11,7 +11,7 @@ function ubuntu_install_web_server()
   sudo mkdir -p /usr/lib/db
   sudo cp db/*.xml /usr/lib/db
   sudo cp ./build/BankApp.fcgi /usr/lib/cgi-bin/
-  sudo chmod +x /usr/lib/cgi-bin/BankApp.fcgi
+  sudo chmod 755 /usr/lib/cgi-bin/BankApp.fcgi
   sudo mkdir -p /etc/apache2/conf.d/
   sudo cp ./server_configs/ubuntu_apache.conf /etc/apache2/conf.d/
   sudo service apache2 restart
