edc2255c28e862907338139c3ed5e98fd3ad15d4
john.doe <robert.hak@tul.cz>
2023-05-10 16:23:18 +0200

    demo trial

--- bank/bank/lib/services/BusinessLogic.cpp
+++ bank/bank/lib/services/BusinessLogic.cpp
@@ -27,9 +27,17 @@ namespace bank::services
     void BusinessLogic::run()
     {
 
-            std::cout << "Content-type: text/html\r\n\r\n";
-            std::cout << "<html><head><title>FastCGI C++ Example</title></head>";
-            std::cout << "<body><h1>Hello, world!</h1></body></html>";
+            FCGX_Request request;
+            FCGX_Init();
+            FCGX_InitRequest(&request, 0, 0);
+
+            while (FCGX_Accept_r(&request) == 0) {
+                    std::cout << "Content-type: text/html\r\n\r\n";
+                    std::cout << "<html><head><title>FastCGI C++ Example</title></head>";
+                    std::cout << "<body><h1>Hello, world!</h1></body></html>";
+
+                    FCGX_Finish_r(&request);
+            }
 
     }
 
