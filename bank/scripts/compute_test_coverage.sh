5699cece7892a2bec3fed04c948f2339672a8985
john.doe <robert.hak@tul.cz>
2023-05-12 14:33:50 +0200

    gcov?

--- bank/scripts/compute_test_coverage.sh
+++ bank/scripts/compute_test_coverage.sh
@@ -4,7 +4,12 @@
 
 function compute_coverage()
 {
-   lcov --capture --directory CMakeFiles/AllTests.dir/tests/  --output-file coverage.info
+    for file in $(find . -name '*.cpp')
+    do
+      gcov "$file"
+    done
+  #CMakeFiles/AllTests.dir/tests/
+   lcov --capture --directory . --capture --output-file coverage.info
    lcov --remove coverage.info '/usr/*' --output-file coverage.info
    lcov --remove coverage.info '../tests/*' --output-file coverage.info
    lcov --list coverage.info --output-file coverage.txt
