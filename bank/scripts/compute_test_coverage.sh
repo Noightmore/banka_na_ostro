3f8991c9ccf8936d2520d9b0901b449f23dbe44f
john.doe <robert.hak@tul.cz>
2023-05-12 13:44:32 +0200

    path update

--- bank/scripts/compute_test_coverage.sh
+++ bank/scripts/compute_test_coverage.sh
@@ -6,7 +6,7 @@ function compute_coverage()
 {
    lcov --capture --directory CMakeFiles/AllTests.dir/tests/  --output-file coverage.info
    lcov --remove coverage.info '/usr/*' --output-file coverage.info
-   lcov --remove coverage.info 'tests/*' --output-file coverage.info
+   lcov --remove coverage.info '../tests/*' --output-file coverage.info
    lcov --list coverage.info --output-file coverage.txt
 }
 
