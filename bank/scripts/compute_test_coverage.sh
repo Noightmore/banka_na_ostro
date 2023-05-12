ed9ef680cc767270e7fa2030d3c731e8bed59016
john.doe <robert.hak@tul.cz>
2023-05-12 17:05:29 +0200

    dependency update

--- bank/scripts/compute_test_coverage.sh
+++ bank/scripts/compute_test_coverage.sh
@@ -7,10 +7,11 @@ function compute_coverage()
     for file in $(find . -name '*.cpp')
     do
         gcov "$file"
+        echo "$file"
     done
 
   #CMakeFiles/AllTests.dir/tests/
-   lcov --capture --directory . --capture --no-checksum --output-file coverage.info
+   lcov --capture --directory . --capture  --output-file coverage.info
    lcov --remove coverage.info '/usr/*' '*/tests/*' '*/pages/*' --output-file coverage.info
    lcov --remove coverage.info  --output-file coverage.info
    lcov --list coverage.info --output-file coverage.txt
