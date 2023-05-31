9f6fc32c9c2712759b70726587c8c6e45acad761
john.doe <robert.hak@tul.cz>
2023-05-31 23:11:24 +0200

    deploy attempt 69.2

--- bank/scripts/deploy.sh
+++ bank/scripts/deploy.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "deploying"
+sudo mkdir -p ~/usr/lib/cgi-bin/
+sudo mkdir -p ~/usr/lib/db
+sudo cp db/*.xml ~/usr/lib/db
+sudo cp ./build/BankApp.cgi ~/usr/lib/cgi-bin/
+sudo chmod 755 ~/usr/lib/cgi-bin/BankApp.cgi
+sudo mkdir -p ~/etc/apache2/conf.d/
+sudo mkdir -p ~/etc/apache2/conf.d/
+sudo cp ./configs/ubuntu_apache.conf ~/etc/apache2/conf.d/
+sudo cp ./configs/msmtprc ~/etc/
