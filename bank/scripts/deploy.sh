9f8920e2774ede9d116b6cef9f3241966c4a0ed6
john.doe <robert.hak@tul.cz>
2023-05-31 23:21:32 +0200

    this should make it work

--- bank/scripts/deploy.sh
+++ bank/scripts/deploy.sh
@@ -4,8 +4,8 @@ echo "deploying"
 ls -al
 sudo mkdir -p ~/usr/lib/cgi-bin/
 sudo mkdir -p ~/usr/lib/db
-sudo cp db/*.xml ~/usr/lib/db
-sudo cp ./build/BankApp.cgi ~/usr/lib/cgi-bin/
+sudo cp banka_na_ostro/bank/db/*.xml ~/usr/lib/db
+sudo cp ~/BankApp.cgi ~/usr/lib/cgi-bin/
 sudo chmod 755 ~/usr/lib/cgi-bin/BankApp.cgi
 sudo mkdir -p ~/etc/apache2/conf.d/
 sudo mkdir -p ~/etc/apache2/conf.d/
