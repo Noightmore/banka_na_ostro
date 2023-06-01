39e461f12efc8143aedb074bf14eaca5548819c1
RH <robert.hak@tul.cz>
2023-06-01 10:17:56 +0000

    Feature/better transfer method (#29)
    * scp used instead of rsync
    
    * scp used instead of rsync workflows?
    
    * tarrrr
    
    * tarrrr
    
    * rsync
    
    * tarball path update
    
    * tarball path update
    
    * tarball path update
    
    * bruh
    
    * bruh
    
    * bruh 2
    
    * path update
    
    * path update

--- bank/scripts/deploy.sh
+++ bank/scripts/deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 echo "deploying"
+mkdir -p /home/azureuser/db_update
+tar –xzf db.tar -C /home/azureuser/bank_update/db_update
 ls -al
 sudo mkdir -p ~/usr/lib/cgi-bin/
 sudo mkdir -p ~/usr/lib/db
-sudo cp banka_na_ostro/bank/db/*.xml ~/usr/lib/db
+sudo cp ~/banka_na_ostro/bank/db/*.xml ~/usr/lib/db
 sudo cp ~/BankApp.cgi ~/usr/lib/cgi-bin/
 sudo chmod 755 ~/usr/lib/cgi-bin/BankApp.cgi
 sudo mkdir -p ~/etc/apache2/conf.d/
