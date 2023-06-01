62bf53c8feab27e8473d538d774d83567574215d
robert <robert.hak@tul.cz>
2023-06-01 08:17:04 +0200

    tarball path update

--- bank/scripts/deploy.sh
+++ bank/scripts/deploy.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 echo "deploying"
-tar –xzf bank.tar -C /home/azure/user/bank
+mkdir -p /home/azureuser/bank_update
+tar –xzf bank.tar -C /home/azureuser/bank_update
 ls -al
 sudo mkdir -p ~/usr/lib/cgi-bin/
 sudo mkdir -p ~/usr/lib/db
-sudo cp banka_na_ostro/bank/db/*.xml ~/usr/lib/db
+sudo cp bank_update/bank/db/*.xml ~/usr/lib/db
 sudo cp ~/BankApp.cgi ~/usr/lib/cgi-bin/
 sudo chmod 755 ~/usr/lib/cgi-bin/BankApp.cgi
 sudo mkdir -p ~/etc/apache2/conf.d/
 sudo mkdir -p ~/etc/apache2/conf.d/
-sudo cp banka_na_ostro/bank/configs/ubuntu_apache.conf ~/etc/apache2/conf.d/
-sudo cp banka_na_ostro/bank/configs/msmtprc ~/etc/
+sudo cp bank_update/bank/configs/ubuntu_apache.conf ~/etc/apache2/conf.d/
+sudo cp bank_update/bank/configs/msmtprc ~/etc/
