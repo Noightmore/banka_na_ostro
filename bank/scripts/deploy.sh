5d59d6f219212b062443bcf32cc1a374ff772426
robert <robert.hak@tul.cz>
2023-06-01 08:38:00 +0200

    bruh

--- bank/scripts/deploy.sh
+++ bank/scripts/deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 echo "deploying"
-mkdir -p /home/azureuser/bank_update
-tar –xzf bank.tar -C /home/azureuser/bank_update
+mkdir -p /home/azureuser/db_update
+tar –xzf db.tar -C /home/azureuser/bank_update/db_update
 ls -al
 sudo mkdir -p ~/usr/lib/cgi-bin/
 sudo mkdir -p ~/usr/lib/db
-sudo cp bank_update/bank/db/*.xml ~/usr/lib/db
+sudo cp bank_na_ostro/bank/db/*.xml ~/usr/lib/db
 sudo cp ~/BankApp.cgi ~/usr/lib/cgi-bin/
 sudo chmod 755 ~/usr/lib/cgi-bin/BankApp.cgi
 sudo mkdir -p ~/etc/apache2/conf.d/
 sudo mkdir -p ~/etc/apache2/conf.d/
-sudo cp bank_update/bank/configs/ubuntu_apache.conf ~/etc/apache2/conf.d/
-sudo cp bank_update/bank/configs/msmtprc ~/etc/
+sudo cp bank_na_ostro/bank/configs/ubuntu_apache.conf ~/etc/apache2/conf.d/
+sudo cp bank_una_ostro/bank/configs/msmtprc ~/etc/
